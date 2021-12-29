082952bc875d03e1021f3dcc0b285295e84c1f7f
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2021-12-29 23:55:21 +0300

    Code to connect MainActivity.java to PlayersPage.java

--- app/src/main/java/com/tictactoe/MainActivity.java
+++ app/src/main/java/com/tictactoe/MainActivity.java
@@ -2,7 +2,9 @@ package com.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void playButtonClicked (View view){
+        Intent openPlayersPage = new Intent(this, PlayersPage.class);
+        startActivity(openPlayersPage);
     }
 }
