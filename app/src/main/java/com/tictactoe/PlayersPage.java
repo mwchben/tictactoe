b8de1985f9871ef1e329519d9e2beab99549fe81
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2021-12-29 23:16:42 +0300

    Added PlayersPage jav+xml and re-colored

---
+++ app/src/main/java/com/tictactoe/PlayersPage.java
@@ -0,0 +1,14 @@
+package com.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayersPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_players_page);
+    }
+}
