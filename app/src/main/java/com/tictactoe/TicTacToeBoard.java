379a6daeb52dcd416168c77e1d522283e5f39f05
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-01-02 21:52:44 +0300

    updated board java file for methods of drawingBoard and onDraw

--- app/src/main/java/com/tictactoe/TicTacToeBoard.java
+++ app/src/main/java/com/tictactoe/TicTacToeBoard.java
@@ -2,6 +2,8 @@ package com.tictactoe;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -14,6 +16,11 @@ public class TicTacToeBoard extends View {
     private final int X_color;
     private final int O_color;
     private final int winning_line_color;
+
+    //
+    private final Paint paintObject = new Paint();
+
+    private int cellSize = getWidth()/3; //can be overridden with the onMeasure method .'.'. down below;
 
     public TicTacToeBoard(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -36,7 +43,24 @@ public class TicTacToeBoard extends View {
         //get square shape, hence use min in maths method
         int dimension = Math.min(getMeasuredHeight(), getMeasuredWidth());
 
+        //cellSize for fitting an X or O gotten by dividing the dimension into 3 parts
+        cellSize = dimension/3;
+
         //set height for min to all the sides of square
         setMeasuredDimension(dimension,dimension);
+    }
+
+    @Override
+    //canvas - area of our view;
+    protected  void  onDraw (Canvas canvas){
+        paintObject.setStyle(Paint.Style.STROKE);
+        paintObject.setAntiAlias(true);
+
+        //a method thar will  actually draw the board
+        drawGameBoard(canvas);
+    }
+
+    private void drawGameBoard(Canvas canvas){
+
     }
 }
